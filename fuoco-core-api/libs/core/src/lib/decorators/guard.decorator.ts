ed5068cbec0b8616f3096e32c7709b8414ddb7ff
lucasfuoco <lucasfuoco@gmail.com>
2022-09-16 14:19:30 -0400

    Add forbidden exception to guard

--- fuoco-core-api/libs/core/src/lib/decorators/guard.decorator.ts
+++ fuoco-core-api/libs/core/src/lib/decorators/guard.decorator.ts
@@ -1,4 +1,5 @@
 import {GuardExecuter} from "../guard-executer";
+import * as HttpError from 'http-errors';
 
 export function Guard<T extends typeof GuardExecuter>(executer: T) {
     return function (
@@ -9,7 +10,7 @@ export function Guard<T extends typeof GuardExecuter>(executer: T) {
         const method = descriptor.value;
         const instance = new executer();
         descriptor.value = async function (...args) {
-            if (await instance.canExecuteAsync()) return;
+            if (await !instance.canExecuteAsync()) throw new HttpError.Forbidden('Not authorized!');
             
             // This part will run when Meteor.isClient == false
             method.apply(this, args);
