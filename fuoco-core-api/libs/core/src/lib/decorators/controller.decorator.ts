b444dc6f11284307e525a661077af10a39a37b16
lucasfuoco <lucasfuoco@gmail.com>
2022-09-17 11:15:21 -0400

    Fix path var assignment is controller decorator

--- fuoco-core-api/libs/core/src/lib/decorators/controller.decorator.ts
+++ fuoco-core-api/libs/core/src/lib/decorators/controller.decorator.ts
@@ -1,7 +1,9 @@
-// deno-lint-ignore-file no-explicit-any ban-types
+// deno-lint-ignore-file no-explicit-any ban-types ban-unused-ignore
+import { IController } from "../controller.ts";
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export function Controller(path: string) {
-    return function (target: new (...args: any[]) => object) {
-      target.prototype.path = path;
+    return function (target: new (...args: any[]) => IController) {
+      target.constructor.prototype.path = path;
     };
 }
