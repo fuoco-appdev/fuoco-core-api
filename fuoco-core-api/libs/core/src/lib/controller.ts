77d2291a946a7d3eea2471e6d0efbc8a631ad3c1
lucasfuoco <lucasfuoco@gmail.com>
2022-09-18 21:10:32 -0400

    Remove controller

--- fuoco-core-api/libs/core/src/lib/controller.ts
+++
@@ -1,3 +0,0 @@
-export class IController {
-    path?: string;
-}
--- fuoco-core-api/libs/core/src/lib/decorators/controller.decorator.ts
+++ fuoco-core-api/libs/core/src/lib/decorators/controller.decorator.ts
@@ -1,11 +1,7 @@
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// deno-lint-ignore-file no-explicit-any ban-types ban-unused-ignore ban-ts-comment
-// @ts-ignore
-import { IController } from "../controller.ts";
+// deno-lint-ignore-file no-explicit-any ban-types
 
 export function Controller(path: string) {
-    return function (target: new (...args: any[]) => IController) {
+    return function (target: new (...args: any[]) => object) {
       target.constructor.prototype.path = path;
     };
 }
