b0f664bd8bbe59435998589f7c0999832cf8d90b
lucasfuoco <lucasfuoco@gmail.com>
2022-09-21 13:56:52 -0400

    Fix compile errors

--- fuoco-core-api/libs/core/src/lib/decorators/guard.decorator.ts
+++ fuoco-core-api/libs/core/src/lib/decorators/guard.decorator.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// deno-lint-ignore-file no-explicit-any no-unused-vars ban-ts-comment
+// deno-lint-ignore-file no-explicit-any no-unused-vars ban-ts-comment ban-unused-ignore
 // @ts-ignore
 import { GuardExecuter } from "../guard-executer.ts";
 // @ts-ignore
 import * as HttpError from "https://deno.land/x/http_errors@3.0.0/mod.ts";
 // @ts-ignore
 import * as Oak from "https://deno.land/x/oak@v11.1.0/mod.ts";
+// @ts-ignore
 import { EndpointContext } from "../endpoint-context.ts";
 
 export function Guard<T extends typeof GuardExecuter>(executer: T) {
