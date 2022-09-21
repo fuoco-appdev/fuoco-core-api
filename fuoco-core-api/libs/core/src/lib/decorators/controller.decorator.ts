559c1b70e3ee5356bdd2c45656efd5ee8b7df8ac
lucasfuoco <lucasfuoco@gmail.com>
2022-09-21 10:38:41 -0400

    Remove return target in controller decorator

--- fuoco-core-api/libs/core/src/lib/decorators/controller.decorator.ts
+++ fuoco-core-api/libs/core/src/lib/decorators/controller.decorator.ts
@@ -4,7 +4,5 @@ export function Controller(path: string) {
     return function (target: new (...args: any[]) => object) {
       const prototype = Object.getPrototypeOf(target);
       prototype.path = path;
-
-      return target;
     };
 }
