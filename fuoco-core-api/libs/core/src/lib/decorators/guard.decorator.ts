9e0e82e32a17875c9f37db98054b33139030c87a
lucasfuoco <lucasfuoco@gmail.com>
2022-09-21 09:37:04 -0400

    fix guard

--- fuoco-core-api/libs/core/src/lib/decorators/guard.decorator.ts
+++ fuoco-core-api/libs/core/src/lib/decorators/guard.decorator.ts
@@ -11,11 +11,9 @@ export function Guard<T extends typeof GuardExecuter>(executer: T) {
         key: string,
         descriptor: PropertyDescriptor,
       ) {
-        const method = descriptor.value;
         const instance = new executer();
-        descriptor.value = async function (...args: any) {
-            if (await !instance.canExecuteAsync()) throw HttpError.createError(401, 'Not authorized!');
-            return target;
-        };
+        instance.canExecuteAsync().then(() => {
+          throw HttpError.createError(401, 'Not authorized!');
+        });
       }
 }
