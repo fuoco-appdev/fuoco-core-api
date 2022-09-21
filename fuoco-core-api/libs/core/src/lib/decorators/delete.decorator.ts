cb06c730651a511df764943d2d03b3ef8dc5fe9f
lucasfuoco <lucasfuoco@gmail.com>
2022-09-21 14:22:17 -0400

    Update all decorators

--- fuoco-core-api/libs/core/src/lib/decorators/delete.decorator.ts
+++ fuoco-core-api/libs/core/src/lib/decorators/delete.decorator.ts
@@ -1,5 +1,8 @@
-// deno-lint-ignore-file no-explicit-any
+// deno-lint-ignore-file no-explicit-any ban-ts-comment
 /* eslint-disable @typescript-eslint/no-explicit-any */
+// @ts-ignore
+import { EndpointContext } from "../endpoint-context.ts";
+
 export function Delete(path: string) {
     return function (
       target: Record<string, any>,
@@ -7,11 +10,12 @@ export function Delete(path: string) {
       descriptor: PropertyDescriptor,
     ) {
       const prototype = Object.getPrototypeOf(target);
-      prototype.deleteEndpoints = prototype.deleteEndpoints || [];
-      prototype.deleteEndpoints.push({
-        path,
-        key,
+      prototype.endpoints = prototype.endpoints ?? {} as Record<string, EndpointContext>;
+      prototype.endpoints[key] = {
+        type: 'delete',
+        path: path,
+        key: key,
         handler: descriptor.value,
-      });
+      };
     };
   }
