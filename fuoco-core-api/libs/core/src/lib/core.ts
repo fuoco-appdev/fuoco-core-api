cb5aa020dfd3389f6690e51edcef527773a49cbd
lucasfuoco <lucasfuoco@gmail.com>
2022-09-30 11:44:05 -0400

    Modify core.ts

--- fuoco-core-api/libs/core/src/lib/core.ts
+++ fuoco-core-api/libs/core/src/lib/core.ts
@@ -36,8 +36,7 @@ import * as HttpError from "https://deno.land/x/http_errors@3.0.0/mod.ts";
       controllers: object[],
     ) {
       for (const controller of controllers) {
-        const prototype = Object.getPrototypeOf(controller.constructor);
-        console.log(controller);
+        const prototype = Object.getPrototypeOf(controller);
         if (!prototype.path) {
             throw new Error(`Controller ${controller.constructor.name} must have a @Controller() decorator!`);
         }
