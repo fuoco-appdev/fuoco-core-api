d254b5e7a1164aeb689edc052e513020083f1d12
lucasfuoco <lucasfuoco@gmail.com>
2023-02-02 19:43:02 -0500

    Update mail service

--- fuoco-core-api/libs/mail/src/lib/services/mail.service.ts
+++ fuoco-core-api/libs/mail/src/lib/services/mail.service.ts
@@ -1,54 +1,81 @@
 // deno-lint-ignore-file ban-ts-comment
 // @ts-ignore
-import { SMTPClient, ClientOptions } from "https://deno.land/x/denomailer@1.5.0/mod.ts";
+import {
+  // @ts-ignore
+  SmtpClient,
+  // @ts-ignore
+  ConnectConfig,
+  // @ts-ignore
+  ConnectConfigWithAuthentication,
+  // @ts-ignore
+} from 'https://deno.land/x/smtp@v0.7.0/mod.ts';
 // @ts-ignore
-import { Handlebars } from "https://deno.land/x/handlebars@v0.8.0/mod.ts";
+import { Handlebars } from 'https://deno.land/x/handlebars@v0.8.0/mod.ts';
 
 export class MailService {
-    private readonly _handlebars: Handlebars;
-    private readonly _config: ClientOptions;
+  private readonly _handlebars: Handlebars;
+  private readonly _config: ConnectConfig | ConnectConfigWithAuthentication;
+  private readonly _tls: boolean;
+  private readonly _client: SmtpClient;
 
-    constructor(config: ClientOptions) {
-        this._config = config;
-        this._handlebars = new Handlebars();
+  constructor(
+    config: ConnectConfig | ConnectConfigWithAuthentication,
+    tls: boolean
+  ) {
+    this._config = config;
+    this._tls = tls;
+    this._client = new SmtpClient();
+    this._handlebars = new Handlebars();
+  }
+
+  public async sendFromHtmlAsync(
+    fromEmail: string,
+    toEmail: string,
+    subject: string,
+    htmlUri: string,
+    context: Record<string, string>
+  ): Promise<void> {
+    const compiledHtml: string = await this._handlebars.render(
+      htmlUri,
+      context
+    );
+
+    if (!this._tls) {
+      await this._client.connect(this._config);
+    } else {
+      await this._client.connectTLS(this._config);
     }
 
-    public async sendFromHtmlAsync(
-        fromEmail: string,
-        toEmail: string,
-        subject: string,
-        htmlUri: string,
-        context: Record<string, string>
-    ): Promise<void> {
-        const compiledHtml: string = await this._handlebars.render(htmlUri, context);
+    await this._client.send({
+      from: fromEmail,
+      to: toEmail,
+      subject: subject,
+      html: compiledHtml,
+      content: '',
+    });
 
-        const client = new SMTPClient(this._config);
+    await this._client.close();
+  }
 
-        await client.send({
-            from: fromEmail,
-            to: toEmail,
-            subject: subject,
-            html: compiledHtml,
-        });
-          
-        await client.close();
+  public async sendFromContentAsync(
+    fromEmail: string,
+    toEmail: string,
+    subject: string,
+    content: string
+  ): Promise<void> {
+    if (!this._tls) {
+      await this._client.connect(this._config);
+    } else {
+      await this._client.connectTLS(this._config);
     }
-    
-    public async sendFromContentAsync(
-        fromEmail: string,
-        toEmail: string,
-        subject: string,
-        content: string,
-    ): Promise<void> {
-        const client = new SMTPClient(this._config);
 
-        await client.send({
-            from: fromEmail,
-            to: toEmail,
-            subject: subject,
-            content: content,
-        });
-          
-        await client.close();
-    }
+    await this._client.send({
+      from: fromEmail,
+      to: toEmail,
+      subject: subject,
+      content: content,
+    });
+
+    await this._client.close();
+  }
 }
